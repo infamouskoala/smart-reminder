d287d643d3e4530f5fe2b75642151bc24c93662b
cointhedev <154434854+cointhedev@users.noreply.github.com>
2024-01-08 18:54:51 +0200

    fix

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import time
+import sys
+from os import system
 import os
 from datetime import datetime
-from src.inject import inject
-from src.notification import notification
-
-def alarm(reminddate, remindtime, note):
+sys.path.append(os.getcwd())
+from inject import inject
+from notification import notification
+def alarm():
     i = 0
-    os.system("clear || cls")
+    file = open("reminders\\reminder.rem", "w")
+    file.write(f"date: {reminddate}\ntime: {remindtime}\nnote: {note}")
+    system("clear || cls")
     print("Reminder is running...")
     while True:
-        print(f"Time elapsed: {i}s", end='\r')
-        i = i + 1
+        print(f"Time elapsed: {i}s", end = '\r')
+        i = i+1
         current_datetime = datetime.now()
         formatted_date = current_datetime.strftime("%d/%m/%y")
         formatted_time = current_datetime.strftime("%H:%M")
@@ -20,29 +24,31 @@ def alarm(reminddate, remindtime, note):
             print(f"{note}")
             input()
             break
+        else:
+            pass
 
-def start(startup):
+def start():
     if startup == "y":
         path = os.path.abspath(__file__)
         inject(path)
         print("Added to autostart")
     elif startup == "n":
-        print("You will have to run the script every time you turn on your pc.")
+        print("You will have to run the script everytime u turn on ur pc.")
 
 # CHOICES + SORTING
 
-os.system("title Reminder Maker")
-os.system("cls || clear")
-
+system("title Reminder Maker")
+system("clear || cls")
 print("""Welcome to Reminder Maker
 This is integrated into your windows so that you can get windows pop up notifications when your reminder goes off
 Date Layout: 00/00/00 (DD/MM/YY)
 Time Layout: HH/MM (24hr)
 """)
-remindtime = input("Time of reminder (HH:MM): ")
+remindtime = input("Time of reminder (HH:MM):  ")
 reminddate = input("Date of reminder (DD/MM/YY): ")
 note = input("Note: ")
-startup = input("Add file to startup (y/n)?").lower()
+startup = input("Add file to startup (y/n)?")
+startup = startup.lower()
 
 if startup == "y":
     inject(os.path.abspath(__file__))
@@ -52,4 +58,4 @@ else:
     print("File not added to startup.")
     time.sleep(3)
 
-alarm(reminddate, remindtime, note)
+alarm()
